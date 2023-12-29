b7a26fd560ad6e0907485c173e8b8ba9d1d429d9
INSEONG-PC\정인성 <twin2688@naver.com>
2023-12-29 14:48:40 +0900

    로그인 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Plogin from "./page/PC/Plogin";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}></Route>
+          <Route path="/Login" element={<Plogin />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
