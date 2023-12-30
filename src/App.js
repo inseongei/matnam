223de805ecfb6f3510afc8b3e3b11682a2126d34
INSEONG-PC\정인성 <twin2688@naver.com>
2023-12-30 18:40:50 +0900

    추가 컴포넌트 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
-import Plogin from "./page/PC/Plogin";
+import Login from "./page/Login";
+import MSign from "./page/Mobile/MSign";
+import Main from "./page/Main";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header />}></Route>
-          <Route path="/Login" element={<Plogin />}></Route>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/SignUp" element={<MSign />}></Route>
         </Routes>
       </BrowserRouter>
     </>
